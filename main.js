71efc77eeb117e123cedd3b8291aedd9ecfc085b
gabriel <dgabrielfof@gmail.com>
2023-11-13 17:26:00 -0300

    adding the fuel comparison

--- main.js
+++ main.js
@@ -1,10 +1,32 @@
 function calculatePrice () {
-  let price = document.getElementById("price").value;
+  let gasoline = document.getElementById("gasoline").value;
+  let alcohol = document.getElementById("alcohol").value;
   let rate = document.getElementById("rate").value;
   let distance = document.getElementById("distance").value;
+  let showFuel = document.getElementById("fuel");
+  let showResult = document.getElementById("result");
+
+  if (gasoline!=="" && alcohol!=="") {
+    if (Number(alcohol)/Number(gasoline <= 0.7)) {
+      price = Number(alcohol);
+      showFuel.innerHTML = "Usar álcool está mais vantajoso"
+    } else {
+      price = Number(gasoline);
+      showFuel.innerHTML = "Usar gasolina está mais vantajoso"
+    }
+  } else if (gasoline!=="" && alcohol==="") {
+    price = Number(gasoline);
+  } else if (gasoline==="" && alcohol!=="") {
+    price = Number(alcohol);
+  } else if (gasoline==="" && alcohol==="") {
+    showFuel.innerHTML = "É necessário ao menos um tipo de conbustível para o cálculo"
+  }
+
   const result = Number(price)/Number(rate)*Number(distance)
-  let showResult = document.getElementById("result")
-  showResult.value = result
-  showResult.innerText = result.toFixed(2)
+  
+  if (!result===NaN) {
+    showResult.value = result
+    showResult.innerText = result.toFixed(2)
+  }
 }
 
